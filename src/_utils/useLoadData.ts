4917155507119a49e59f3a31e0dd6a5fd260e717
RieNishi <r.nishi@liveness.co.jp>
2022-10-21 13:37:42 +0900

    コメントアウト解除

--- src/_utils/useLoadData.ts
+++ src/_utils/useLoadData.ts
@@ -24,8 +24,8 @@ export function useLoadData<T>(initUrl: string, initialData: T | undefined) {
     setIsLoading(true);
     try {
       const result = await get<T>(url);
-      // console.log(url); // TODO: debug
-      // console.log(result.parsedBody); // TODO: debug
+      console.log(url); // TODO: debug
+      console.log('get', result.parsedBody); // TODO: debug
       if (result.parsedBody) setData(result.parsedBody);
     } catch (error) {
       setIsError(true);
