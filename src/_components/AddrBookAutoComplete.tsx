7e7db2bf900d04524f6e592576482b74cc940f17
RieNishi <r.nishi@liveness.co.jp>
2023-01-12 15:00:46 +0900

    アドレス帳の自由入力対応

--- src/_components/AddrBookAutoComplete.tsx
+++ src/_components/AddrBookAutoComplete.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Control, Controller, DeepMap, DeepPartial, FieldError, FieldValues, Path } from 'react-hook-form';
 
-import { Autocomplete } from '@material-ui/lab';
-import { CircularProgress, TextField } from '@material-ui/core';
+import { Autocomplete, createFilterOptions } from '@material-ui/lab';
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, TextField } from '@material-ui/core';
 import { CSSProperties } from '@material-ui/styles';
 
 import { EmailAddress } from '_models/User';
 import { useLoadData } from '_utils/useLoadData';
 import { MyChip } from './MyChip';
+
+const createFilter = createFilterOptions<EmailAddress>();
 
 type AddrBookAutoCompleteType<TFieldValues extends FieldValues, TName extends Path<TFieldValues>> = {
   name: TName;
@@ -37,19 +39,32 @@ export function AddrBookAutoComplete<TFieldValues extends FieldValues, TName ext
     }
   }, [filter, setUrl]);
 
-  // アドレス帳検索の状態
+  // 追加アドレスダイアログの状態
   const [open, setOpen] = useState(false);
 
-  // アドレス帳検索の初期化
-  useEffect(() => {
-    if (!open) {
-      setFilter('');
-    }
-  }, [open]);
+  // 追加アドレスの状態
+  const [dialogValue, setDialogValue] = useState<EmailAddress>({
+    name: '',
+    address: '',
+  });
 
   // アドレス帳検索値のリアルタイム取得
   const onChangeHandle = async (value: string) => {
     setFilter(value);
+  };
+
+  const handleDialogClose = () => {
+    setDialogValue({ name: '', address: '' });
+    setOpen(false);
+  };
+
+  const handleAddClick = () => {
+    // setValue({
+    //   name: dialogValue.name,
+    //   address: dialogValue.address,
+    // });
+    console.log(dialogValue); //debug
+    handleDialogClose();
   };
 
   return (
@@ -57,52 +72,93 @@ export function AddrBookAutoComplete<TFieldValues extends FieldValues, TName ext
       name={name}
       control={control}
       render={({ field }) => (
-        <Autocomplete
-          {...field}
-          disabled={disabled}
-          style={style}
-          multiple
-          limitTags={disabled ? undefined : 2}
-          size="small"
-          open={open}
-          onOpen={() => {
-            setOpen(true);
-          }}
-          onClose={() => {
-            setOpen(false);
-          }}
-          options={!!addressbook && !!filter ? addressbook : []}
-          filterOptions={(options, _state) => options}
-          noOptionsText={t('auto-complete.no-options-text')}
-          loading={loading}
-          getOptionLabel={(option) => `${option.name} <${option.address}>`}
-          renderTags={(tagValue, getTagProps) => tagValue.map((option, index) => <MyChip option={option} {...getTagProps({ index })} />)}
-          filterSelectedOptions
-          getOptionSelected={(option, value) => option.address === value.address}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label={label}
-              error={!!errors[name]}
-              helperText={errors[name] && (errors[name] as FieldError).message}
-              InputProps={{
-                ...params.InputProps,
-                endAdornment: (
-                  <>
-                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                    {params.InputProps.endAdornment}
-                  </>
-                ),
-              }}
-              onChange={(ev) => {
-                if (ev.target.value !== '' || ev.target.value !== null) {
-                  onChangeHandle(ev.target.value);
-                }
-              }}
-            />
-          )}
-          onChange={(_, data) => field.onChange(data)}
-        />
+        <>
+          <Autocomplete
+            {...field}
+            disabled={disabled}
+            style={style}
+            freeSolo
+            multiple
+            limitTags={disabled ? undefined : 2}
+            size="small"
+            options={!!addressbook && !!filter ? addressbook : []}
+            filterOptions={(options, params) => {
+              const filtered = createFilter(options, params) as EmailAddress[];
+              if (params.inputValue !== '') {
+                filtered.push({
+                  name: params.inputValue,
+                  address: params.inputValue,
+                });
+              }
+              return filtered;
+            }}
+            noOptionsText={t('auto-complete.no-options-text')}
+            loading={loading}
+            getOptionLabel={(option) => (!!option.address ? `${option.name} <${option.address}>` : `${option.name}`)}
+            renderTags={(tagValue, getTagProps) => tagValue.map((option, index) => <MyChip option={option} {...getTagProps({ index })} />)}
+            filterSelectedOptions
+            getOptionSelected={(option, value) => option.address === value.address}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label={label}
+                error={!!errors[name]}
+                helperText={errors[name] && (errors[name] as FieldError).message}
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <>
+                      {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                      {params.InputProps.endAdornment}
+                    </>
+                  ),
+                }}
+                onChange={(ev) => {
+                  if (ev.target.value !== '' || ev.target.value !== null) {
+                    onChangeHandle(ev.target.value);
+                  }
+                }}
+              />
+            )}
+            onChange={(_, data) => {
+              // if (typeof data === 'string') {
+              //   // timeout to avoid instant validation of the dialog's form.
+              //   setTimeout(() => {
+              //     setOpen(true);
+              //     setDialogValue({ name: data, address: '' });
+              //   });
+              // } else if (data && data.inputvalue) {
+              //   setOpen(true);
+              //   setDialogValue({
+              //     name: data.name,
+              //     address: '',
+              //   });
+              // } else {
+              field.onChange(data);
+              // }
+            }}
+          />
+          <Dialog open={open} onClose={handleDialogClose} aria-labelledby="form-dialog-title">
+            <DialogContent>
+              <DialogContentText>Did you miss any film in our list? Please, add it!</DialogContentText>
+              <TextField
+                label={t('auto-complete.add-dialog.name')}
+                value={dialogValue.name}
+                onChange={(event) => setDialogValue({ ...dialogValue, name: event.target.value })}
+                autoFocus
+              />
+              <TextField
+                label={t('auto-complete.add-dialog.address')}
+                value={dialogValue.address}
+                onChange={(event) => setDialogValue({ ...dialogValue, address: event.target.value })}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDialogClose}>{t('common.button.cancel')}</Button>
+              <Button onClick={handleAddClick}>{t('common.button.ok')}</Button>
+            </DialogActions>
+          </Dialog>
+        </>
       )}
     />
   );
