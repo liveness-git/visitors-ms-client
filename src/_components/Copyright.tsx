a8249806fd38fcf71e746a99ba222002ae16fd5e
RieNishi <r.nishi@liveness.co.jp>
2023-10-25 20:16:25 +0900

    Version 1.7.0

--- src/_components/Copyright.tsx
+++ src/_components/Copyright.tsx
@@ -8,7 +8,7 @@ import { Link, Typography } from '@material-ui/core';
 export function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Version 1.6.0 - Copyright © '}
+      {'Version 1.7.0 - Copyright © '}
       <Link color="inherit" href="https://www.liveness.co.jp/">
         liveness, Inc.
       </Link>{' '}
